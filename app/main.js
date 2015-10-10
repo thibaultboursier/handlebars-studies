5504c00951e1e6ac3a91167004edcfe4e963ae30
thibaultboursier <boursier.thibault@gmail.com>
2015-10-10 15:07:00 +0200

    add bootstrapJS to add collapses on each repositories list-item

--- app/main.js
+++ app/main.js
@@ -1,15 +1,21 @@
 require.config({
+    shim: {
+        "bootstrap": {
+            "deps": ['jquery']
+        }
+    },
     paths: {
         "jquery": "../bower_components/jquery/dist/jquery.min",
+        "bootstrap": "../bower_components/bootstrap/dist/js/bootstrap.min",
+        "handlebars": "../bower_components/handlebars/handlebars.min",
         "underscore": "../bower_components/underscore/underscore-min",
         "backbone": "../bower_components/backbone/backbone-min",
-        "handlebars": "../bower_components/handlebars/handlebars.min",
         "app": "assets/js/app",
         "modules": "assets/js/modules"
     }
 });
 
-require(['jquery', 'underscore', 'backbone'], function () {
+require(['jquery', 'underscore', 'backbone', 'bootstrap'], function () {
     require(['app'], function (App) {
         App.init();
     });
